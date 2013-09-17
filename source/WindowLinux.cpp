cdbb47e8dbc4d16b71c560d77d3d3a3f65bba4f1
doqunbop <deadlyak@gmail.com>
2013-09-17 16:41:26 +0200

    Typos.

--- source/WindowLinux.cpp
+++ source/WindowLinux.cpp
@@ -4,9 +4,9 @@ WindowLinux::WindowLinux()
 	: WindowBaseClass()
 {
 	m_renderComponentInterface = new RenderComponentLinux();
-  if( !(RenderComponentLinux)m_renderComponentInterface->Init() )
+  if( !((RenderComponentLinux)m_renderComponentInterface)->Init() )
   {
-    printf( (RenderComponentLinux)m_renderComponentInterface->GetErrorMessage() );
+    printf( ((RenderComponentLinux)m_renderComponentInterface)->GetErrorMessage() );
     exit(1);
   }
 	m_gameInterface->Initialize(m_renderComponentInterface);
