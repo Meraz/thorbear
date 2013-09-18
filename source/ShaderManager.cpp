e719531b5a4524b206f09bc97e26f65f78ffa7d5
Rasmus Tilljander <tilljander.rasmus@gmail.coM>
2013-09-18 14:43:19 +0200

    Moved files

---
+++ source/ShaderManager.cpp
@@ -0,0 +1,37 @@
+#include "ShaderManager.h"
+
+
+ShaderManager::ShaderManager()
+{
+}
+
+ShaderManager::~ShaderManager()
+{
+}
+
+void ShaderManager::Init( ID3D11Device* p_d3dDevice, ID3D11DeviceContext* p_context )
+{
+	m_d3dDevice = p_d3dDevice;
+	m_context = p_context;
+}
+
+void ShaderManager::AddShader(char* p_FXFileName, int p_shaderFlags)
+{
+	m_shaderList.push_back(new Shader());
+	m_shaderList.at(m_shaderList.size()-1)->Init(m_d3dDevice, m_context, p_FXFileName, p_shaderFlags);
+}
+
+Shader* ShaderManager::GetShaderByName(string p_FXFileName)
+{
+	for (int i = 0; i < (int)m_shaderList.size(); i++)
+	{
+		if(m_shaderList[i]->GetFXFileName() == p_FXFileName)
+		{
+			return m_shaderList[i];
+		}
+
+	}
+	return m_shaderList[0];
+}
+
+
