bf4fcd53b220fa9d9ceec4331747ede841d0ef36
doqunbop <deadlyak@gmail.com>
2013-09-19 14:58:33 +0200

    Updated basic primitive test to match shader required data.

--- source/RenderComponentLinux.cpp
+++ source/RenderComponentLinux.cpp
@@ -116,6 +116,21 @@ bool RenderComponentLinux::Init()
   l_cam.SetFoV( 45.f );
   l_cam.UpdateProjectionMatrix( );
   m_genericShader.SetActiveCamera( l_cam );
+  
+  float vertices[] = {
+      0.f, 6.f, 10.f, 0.f, 0.f, 0.f, 0.f, 1.f,
+      -6.f, -6.f, 10.f, 0.f, 0.f, 0.f, 0.f, 1.f,
+      6.f, -6.f, 10.f, 0.f, 0.f, 0.f, 0.f, 1.f
+    };
+  GLuint l_vbo, l_vao;
+  glGenVertexArrays(1, &l_vao);
+  glBindVertexArray(l_vao);
+  glGenBuffers(1, &l_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, l_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8, (const GLvoid *)0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8, (const GLvoid *)(sizeof(float)*3));
+  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8, (const GLvoid *)(sizeof(float)*5));
 	
   return true;
 }
@@ -147,14 +162,7 @@ void RenderComponentLinux::Render()
   m_genericShader.Use( );
   
   // Render here
-  glBegin(GL_TRIANGLES);
-  glColor3f(1.f, 0.f, 0.f);
-  glVertex3f(-0.6f, -0.4f, -1.f);
-  glColor3f(0.f, 1.f, 0.f);
-  glVertex3f(0.6f, -0.4f, -1.f);
-  glColor3f(0.f, 0.f, 1.f);
-  glVertex3f(0.f, 0.6f, -1.f);
-  glEnd();
+  glDrawArrays( GL_TRIANGLES, 0, 3 );
   
   glfwSwapBuffers();
 }
