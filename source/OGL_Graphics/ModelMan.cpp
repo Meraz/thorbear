21454ffff035a6b4246bda0369463d0d282f659a
doqunbop <deadlyak@gmail.com>
2013-09-20 16:03:49 +0200

    Fix for ModelMan.

--- source/OGL_Graphics/ModelMan.cpp
+++ source/OGL_Graphics/ModelMan.cpp
@@ -1,6 +1,7 @@
 #include "ModelMan.h"
 
 #include "glm/glm.hpp"
+#include "glm/ext.hpp"
 
 ModelMan::ModelMan( )
 {
@@ -12,19 +13,19 @@ ModelMan::~ModelMan( )
 
 void LoadModels( )
 {
-  model[ ENEMY1 ].Load( MODEL_DIR, "Enemy1.obj");
-  model[ ENEMY2 ].Load( MODEL_DIR, "Enemy2.obj");
-  model[ BALL ].Load( MODEL_DIR, "Ball.obj");
-  model[ PADDLE ].Load( MODEL_DIR, "Paddle.obj");
+  m_models[ ENEMY1 ].Load( MODEL_DIR, "Enemy1.obj");
+  m_models[ ENEMY2 ].Load( MODEL_DIR, "Enemy2.obj");
+  m_models[ BALL ].Load( MODEL_DIR, "Ball.obj");
+  m_models[ PADDLE ].Load( MODEL_DIR, "Paddle.obj");
 }
 
 Model* CreateInstance( BoundingBox p_boundingBox, TextureType p_textureType )
 {
   glm::mat4 l_modelMatrix = glm::translate(
-      p_boundingBox.farTopLeftX + width/2.f,
-      p_boundingBox.farTopLeftY + height/2.f,
+      p_boundingBox.farTopLeftX + p_boundingBox.width/2.f,
+      p_boundingBox.farTopLeftY + p_boundingBox.height/2.f,
       0
     );
     
-  return new ModelInstance( model[ p_textureType ], l_modelMatrix );
+  return new ModelInstance( models[ p_textureType ], l_modelMatrix );
 }
