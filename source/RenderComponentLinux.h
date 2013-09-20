b4870b7bd8255716a0bf9e7de8b67e7792f821bc
doqunbop <deadlyak@gmail.com>
2013-09-20 15:27:01 +0200

    Fix typo.

--- source/RenderComponentLinux.h
+++ source/RenderComponentLinux.h
@@ -3,7 +3,7 @@
 
 #include "RenderComponentInterface.h"
 #include "OGL_Graphics/Shader.h"
-#include "OGL_Graphics/ModelManager.h"
+#include "OGL_Graphics/ModelMan.h"
 
 #include <GL/glew.h> // always include GLEW before including freeGLUT so that you get the latest opengl headers and not the default ones
 #include <GL/glfw.h>
