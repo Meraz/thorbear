1738ee5a56e85160479bcf4354230a150361e2bb
doqunbop <deadlyak@gmail.com>
2013-09-24 14:43:24 +0200

    Syntax fix.

--- resources/OGLShaders/genericVertex.glsl
+++ resources/OGLShaders/genericVertex.glsl
@@ -39,7 +39,7 @@ void main()
 	normal = normalize( normalMatrix * vertexNormal );
 	uv = vertexUV;
 
-	eyeCoords = modelViewMatrix * vec4( vertexPosition * gl2dx, 1.0f );
+	eyeCoords = modelViewMatrix * (vec4( vertexPosition, 1.0f ) * gl2dx);
 
 	lightVec = vec3( viewMatrix * lightPosition - eyeCoords );
 	diffuse = max( 0.0f, dot( normalize( lightVec ), normal ) ) * intensityDiffuse;
