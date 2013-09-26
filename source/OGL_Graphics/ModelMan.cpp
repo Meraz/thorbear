2d280c6c64e894ff01ff5e8e99aa881feeda773c
doqunbop <deadlyak@gmail.com>
2013-09-26 11:19:43 +0200

    Fix for OGL model scaling.

--- source/OGL_Graphics/ModelMan.cpp
+++ source/OGL_Graphics/ModelMan.cpp
@@ -22,10 +22,10 @@ void ModelMan::LoadModels( )
 ModelInstance* ModelMan::CreateInstance( BoundingBox p_boundingBox, TextureType p_textureType )
 {
   glm::mat4 l_modelMatrix = glm::translate(
-      p_boundingBox.PosX + p_boundingBox.Width/2.f,
-      p_boundingBox.PosY + p_boundingBox.Height/2.f,
-      0.f
-    ) * glm::scale(glm::mat4(1.f), glm::vec3(p_boundingBox.Width, p_boundingBox.Height, 1.f));
+      p_boundingBox.PosX + p_boundingBox.Width*0.5f,
+      p_boundingBox.PosY + p_boundingBox.Height*0.5f,
+      p_boundingBox.Depth*0.5f
+    ) * glm::scale(glm::mat4(1.f), glm::vec3(p_boundingBox.Width*0.5f, p_boundingBox.Height*0.5f, p_boundingBox.Depth*0.5f));
     
   return new ModelInstance( &m_models[ p_textureType ], l_modelMatrix );
 }
