d67da4ad0f949fa07f6bf81d05f639e1f813e97e
unknown <BTH@.students.ad.bth.se>
2013-09-26 09:43:34 +0200

    Senap ar kass och glommer enemySquad filerna

---
+++ source/EnemySquad.cpp
@@ -0,0 +1,143 @@
+#include "EnemySquad.h"
+
+EnemySquad::EnemySquad()
+{
+	m_velocity = 0;
+	m_BoundingBox = BoundingBox(0,0,0,0);
+}
+
+EnemySquad::~EnemySquad()
+{
+	vector<Enemy*>().swap(m_enemy);
+	vector<Laser*>().swap(m_laser);
+}
+
+void EnemySquad::Init(BoundingBox p_mapEdges, float p_velocity, vector<Enemy*> p_enemy)
+{
+	m_mapEdges = p_mapEdges;
+	m_velocity = p_velocity;
+	m_enemy = p_enemy;
+	m_currentEnemyDirection = HORIZONTAL;
+	m_currentEnemyY = FindLowestEnemyRow();
+}
+
+void EnemySquad::Update( float p_deltaTime )
+{
+	MoveEnemies(p_deltaTime);
+	HandleLaserFiring();
+
+	for(unsigned int i = 0; i < m_laser.size(); i++)
+		m_laser.at(i)->Update(p_deltaTime);
+}
+
+void EnemySquad::MoveEnemies( float p_deltaTime)
+{
+	switch(m_currentEnemyDirection)
+	{
+	case HORIZONTAL:
+		for(unsigned int i = 0; i < m_enemy.size(); i++)
+		{
+			m_enemy.at(i)->Update(m_velocity * p_deltaTime, m_currentEnemyDirection);
+			if(m_enemy.at(i)->GetBoundingBox().PosX <= m_mapEdges.PosX || 
+				m_enemy.at(i)->GetBoundingBox().PosX + m_enemy.at(i)->GetBoundingBox().Width >= m_mapEdges.PosX + m_mapEdges.Width)
+			{
+				m_velocity *= -1;
+				m_currentEnemyDirection = VERTICAL;
+				m_currentEnemyY = FindLowestEnemyRow(); //Use the lowest row so that we can use the same variable for laser firing checks
+				m_targetY = m_currentEnemyY - m_enemy.at(0)->GetBoundingBox().Height;
+
+			}
+		}
+		break;
+	case VERTICAL:
+		m_currentEnemyY -= abs(m_velocity * p_deltaTime);
+		if(m_currentEnemyY < m_targetY)
+		{
+			m_currentEnemyY = m_targetY; //Helps for checking which enemies will fire lasers
+			m_currentEnemyDirection = HORIZONTAL;
+			break;
+		}
+		for(unsigned int i = 0; i < m_enemy.size(); i++)
+		{
+			m_enemy.at(i)->Update(abs(m_velocity * p_deltaTime), m_currentEnemyDirection);
+
+		}
+		break;
+	}
+}
+
+float EnemySquad::FindLowestEnemyRow()
+{
+	float l_lowestRow = 1000;
+	for(unsigned int i = 0; i < m_enemy.size(); i++)
+	{
+		if(m_enemy.at(i)->GetBoundingBox().PosY < l_lowestRow) //The lower the Y value, the lower the row 
+			l_lowestRow = m_enemy.at(i)->GetBoundingBox().PosY;
+	}
+
+	return l_lowestRow;
+}
+
+void EnemySquad::HandleLaserFiring()
+{
+	for(unsigned int i = 0; i < m_enemy.size(); i++)
+	{
+		if(m_enemy.at(i)->WantsToFire())
+			if(m_enemy.at(i)->GetBoundingBox().PosY <= m_currentEnemyY) //Make sure that only the enemies in the lowest row gets to fire
+			{
+				Laser* temp = new Laser();
+				BoundingBox box(m_enemy.at(i)->GetBoundingBox().PosX, m_enemy.at(i)->GetBoundingBox().PosY);
+				box.Height = 10;
+				box.Width = 2;
+				temp->Init(m_renderComp, 200, box); //TODO Don't hard code velocity in the end
+				m_laser.push_back(temp);
+			}
+	}
+}
+
+void EnemySquad::Render()
+{
+	for(unsigned int i = 0; i < m_enemy.size(); i++)
+		m_enemy.at(i)->Render();
+
+	for(unsigned int i = 0; i < m_laser.size(); i++)
+		m_laser.at(i)->Render();
+}
+
+void EnemySquad::SetSquadRenderComponent( RenderComponentInterface* p_renderComp )
+{
+	m_renderComp = p_renderComp;
+	for(unsigned int i = 0; i < m_enemy.size(); i++)
+	{
+		m_enemy.at(i)->SetRenderComponentInterface(p_renderComp);
+	}
+}
+
+BoundingBox EnemySquad::GetBoundingBox()
+{
+	return m_BoundingBox;
+}
+
+int EnemySquad::GetNumOfEnemies()
+{
+	return m_enemy.size();
+}
+
+vector<Enemy*> EnemySquad::GetEnemies()
+{
+	return m_enemy;
+}
+
+vector<Laser*> EnemySquad::GetLasers()
+{
+	return m_laser;
+}
+
+void EnemySquad::EraseMember( int p_type, int p_vectorPos )
+{
+	if(p_type == BALL) //TODO Change to LASER once this define is implemented
+		m_laser.erase(m_laser.begin() + p_vectorPos);
+	else if(p_type == ENEMY1) //only use ENEMY1 for both types
+		m_enemy.erase(m_enemy.begin() + p_vectorPos);
+}
+
---
+++ source/EnemySquad.h
@@ -0,0 +1,41 @@
+#ifndef ENEMYSQUAD_H
+#define ENEMYSQUAD_H
+
+#include "ShootingEnemy.h"
+#include "DefensiveEnemy.h"
+#include "Laser.h"
+#include <vector>
+
+class EnemySquad
+{
+public:
+	EnemySquad();
+	~EnemySquad();
+	void Init(BoundingBox p_mapEdges, float p_velocity, vector<Enemy*> p_enemy);
+	void Update(float p_deltaTime);
+	void Render();
+	void SetSquadRenderComponent(RenderComponentInterface* p_renderComp);
+	BoundingBox GetBoundingBox();
+	int GetNumOfEnemies();
+	vector<Enemy*> GetEnemies();
+	vector<Laser*> GetLasers();
+	void EraseMember(int p_type, int p_vectorPos);
+private:
+	RenderComponentInterface*	m_renderComp;
+	vector<Enemy*>				m_enemy;
+	vector<Laser*>				m_laser;
+	BoundingBox					m_BoundingBox;
+	BoundingBox					m_mapEdges;
+	float						m_velocity;
+	float						m_targetY; //used to check how far down the enemies have moved in one go
+	float						m_currentEnemyY; //dynamic variable for above check
+
+	EnemyDirection				m_currentEnemyDirection;
+private:
+
+	void	MoveEnemies(float p_deltaTime);
+	float	FindLowestEnemyRow();
+	void	HandleLaserFiring();
+};
+
+#endif
