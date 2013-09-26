04e28f447ad0aa1510f0f3e3a7ef55c91ae46328
doqunbop <deadlyak@gmail.com>
2013-09-26 12:05:56 +0200

    Remove centering of objects in OGL.

--- source/OGL_Graphics/ModelMan.cpp
+++ source/OGL_Graphics/ModelMan.cpp
@@ -25,8 +25,8 @@ ModelInstance* ModelMan::CreateInstance( BoundingBox p_boundingBox, TextureType 
     p_boundingBox.Depth = 1;
     
   glm::mat4 l_modelMatrix = glm::translate(
-      p_boundingBox.PosX - p_boundingBox.Width*0.5f,
-      p_boundingBox.PosY - p_boundingBox.Height*0.5f,
+      p_boundingBox.PosX, // - p_boundingBox.Width*0.5f,
+      p_boundingBox.PosY, // - p_boundingBox.Height*0.5f,
       0 - p_boundingBox.Depth*0.5f
     ) * glm::scale(glm::mat4(1.f), glm::vec3(p_boundingBox.Width, p_boundingBox.Height, p_boundingBox.Depth));
     
