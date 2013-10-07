6d10a965f675d2f1c26962a0bab640b0c4aae0ef
doqunbop <deadlyak@gmail.com>
2013-10-07 16:52:01 +0200

    Fix output value of fontFragment shader.

--- effect/fontFragment.glsl
+++ effect/fontFragment.glsl
@@ -4,7 +4,9 @@ uniform sampler2D tex;
 uniform vec4 color;
 
 in vec2 texcoord;
+
+out vec4 fColor;
  
 void main(void) {
-  gl_FragColor = vec4(vec3(1), texture2D(tex, texcoord).a) * color;
+  fColor = vec4(vec3(1), texture2D(tex, texcoord).a) * color;
 }
