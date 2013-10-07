d6ea9ef5b9abd876c51e29382c72d4b281389808
Erik/Wingly <Erik_the_great_one@hotmail.com>
2013-10-07 14:59:41 +0200

    Highscore should be funtional logic-wise now, need buttons however

--- source/HighscoreScene.h
+++ source/HighscoreScene.h
@@ -1,25 +1,33 @@
 #ifndef HIGHSCORESCENE_H
 #define HIGHSCORESCENE_H
 
-#include <iostream>
 #include <fstream>
 #include <string>
-
-
+#include <sstream>
+#define MAX_HIGHSCORE_ENTRIES 10
 class HighscoreScene
 {
 public:
 	HighscoreScene();
 	~HighscoreScene();
-	void Init();
+	void Init(int p_PlayerScore);
 	void Update();
 	void Render();
+	void NextChar(int p_buttonId);
+	void PrevChar(int p_buttonId);
+	void SaveFile();
 private:
 	void LoadFile();
+	void CreateBaseHighscore();
+	
+	void InsertScoreToHighscore();
 	void WriteFile();
 
 private:
-	std::string m_highscoreData[10][2];
+	std::string m_highscoreData[MAX_HIGHSCORE_ENTRIES][2]; // slot 1 = name slot 2 = score
+	std::string m_playerName;
+	int			m_playerScore;
+	bool		m_newAdditionToHighscore;
 
 };
 #endif // !HIGHSCORESCENE_H
